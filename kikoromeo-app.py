ce5c22e0854cc3b830ba1cebffaa4d676deda2e4
rodneymawero <rodneymawero@gmail.com>
2024-01-29 13:00:16 +0300

    Deleted redundant code

--- kikoromeo-app.py
+++ kikoromeo-app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import LabelBinarizer
 
 # Load the pickled model
 with open('catboost_clf.pkl', 'rb') as f:
